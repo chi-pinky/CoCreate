0335a808ae648a6f495b07cc1d14c2ba5a3e2834
Daniella <149641643+chi-pinky@users.noreply.github.com>
2025-05-16 23:21:15 +0100

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,31 @@
+# Question 1
+# What is the capital city of Japan?
+# A. Seoul
+# B. Beijing
+# C. Tokyo
+# D. Bangkok
+# ✅ Correct Answer: C. Tokyo
+
+# Question 2
+# Who wrote the play Romeo and Juliet?
+# A. Charles Dickens
+# B. William Shakespeare
+# C. Mark Twain
+# D. Jane Austen
+# ✅ Correct Answer: B. William Shakespeare
+
+# Question 3
+# What is the capital of France?
+# A. Paris
+# B. Beijing
+# C. Tokyo
+# D. Bangkok
+# ✅ Correct Answer: A. Paris
+
+# Question 4
+# What is the capital of Germany?
+# A. Seoul
+# B. Beijing
+# C. Tokyo
+# D. Bangkok
+# ✅ Correct Answer: B. Beijing
